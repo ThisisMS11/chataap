1201729af67e724b252ebd7e2562b97311b12b9b
Mohit Saini <mailtomohit2002@gmail.com>
2023-07-19 09:28:25 +0530

    converted into pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,45 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
+
+const manifestForPlugin: Partial<VitePWAOptions> = {
+  registerType: 'prompt',
+  includeAssets: ['vite.svg'],
+  manifest: {
+    name: "Chataap",
+    short_name: "cp",
+    description: "a normal chat application",
+    icons: [
+      {
+        src: "/public/maskable_icon.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable"
+      },
+      {
+        src: "/public/maskable_icon.png",
+        sizes: "128x128",
+        type: "image/png",
+        purpose: "any"
+      },
+      {
+        src: "/public/maskable_icon.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "any maskable"
+      }
+    ],
+    theme_color: "#ffffff",
+    background_color: "#000000",
+    display: 'standalone',
+    scope: '/',
+    start_url: '/',
+    orientation: 'portrait'
+  }
+}
+
 export default defineConfig({
-  plugins: [react()],
+  base: "./",
+  plugins: [react(), VitePWA(manifestForPlugin)],
 })
